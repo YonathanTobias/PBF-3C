840b718b834c1be6c4ae06287521168c1e7b6910
YonathanTobias <yonathantobias@gmail.com>
2020-03-06 11:22:22 +0700

    modul 3

---
+++ Modul 3/src/container/StateFullComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class StateFullComponent extends React.Component
+{
+    render(){
+    return <p>Ini adalah Statefull Component</p>
+    }
+}
+
+//agar component ini dapat dipakai dimana aja
+export default StateFullComponent;
